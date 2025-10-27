c605225cbe9ad726cc0a2da773989e2bdc78d78e
AlexanderTejeda <alexanderd.tejeda.08@gmail.com>
2025-10-27 14:07:16 -0600

    fix analytics vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import LandingPage from "./pages/LandingPage";
 import FloatingContactButton from "./components/FloatingContacButton";
-import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <>
       <LandingPage />
       <FloatingContactButton />
-      <Analytics />
     </>
   );
 
