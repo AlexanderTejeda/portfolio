7b943c4d79a95bea5b9315f170a71452bcc4fa2f
AlexanderTejeda <alexanderd.tejeda.08@gmail.com>
2025-10-27 14:03:15 -0600

    analytics vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import LandingPage from "./pages/LandingPage";
 import FloatingContactButton from "./components/FloatingContacButton";
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <>
       <LandingPage />
       <FloatingContactButton />
+      <Analytics />
     </>
   );
 
